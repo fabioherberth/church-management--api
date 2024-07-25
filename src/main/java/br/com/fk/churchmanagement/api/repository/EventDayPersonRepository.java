65a8a1f42406af3b716ad9852f4af11c45fb4fa7
fabioherberth <fabioherberth@hotmail.com>
2024-07-25 18:28:53 -0300

    A
    Ajustando a query do SQL.

--- src/main/java/br/com/fk/churchmanagement/api/repository/EventDayPersonRepository.java
+++ src/main/java/br/com/fk/churchmanagement/api/repository/EventDayPersonRepository.java
@@ -18,7 +18,7 @@ public interface EventDayPersonRepository extends JpaRepository<EventDayPerson, 
     List<Long> checkAttendanceOnTheDay(@Param("peopleIds") List<Long> peopleIds);
 
     @Query(value = """
-            SELECT p."name" AS name
+            SELECT p.name AS name
               FROM event_day_person edp
              INNER JOIN person p on edp.person_id = p.id
              WHERE DATE(edp.event_day) = DATE(NOW())
